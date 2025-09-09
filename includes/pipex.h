8cbfd1288d51303d7f1cc6e0462369618affdcee
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2025-09-09 12:51:37 +0200

    v0.2 first tries execve

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: cecompte <cecompte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 11:15:56 by cecompte          #+#    #+#             */
-/*   Updated: 2025/09/09 11:54:12 by cecompte         ###   ########.fr       */
+/*   Updated: 2025/09/09 12:31:14 by cecompte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include "../libft/includes/libft.h"
 # include <sys/wait.h>
 
-char	**find_path(char **envp);
+char	*find_path(char *cmd, char **envp);
+char	*build_path(char *src, char *cmd);
+void	free_tab(char **tab);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,50 @@
 /*   By: cecompte <cecompte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 11:13:28 by cecompte          #+#    #+#             */
-/*   Updated: 2025/09/09 12:01:04 by cecompte         ###   ########.fr       */
+/*   Updated: 2025/09/09 12:49:35 by cecompte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	**find_path(char **envp)
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	if (!tab)
+		return ;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	tab = NULL;
+}
+
+char	*build_path(char *src, char *cmd)
+{
+	char	*path;
+	int		len_src;
+	int		len_cmd;
+
+	len_src = ft_strlen(src);
+	len_cmd = ft_strlen(cmd); // cmd = NULL ?
+	path = malloc(len_src + len_cmd + 2);
+	if (!path)
+		return (NULL);
+	ft_strlcpy(path, src, len_src + 1);
+	ft_strlcat(path, "/", len_src + 2);
+	ft_strlcat(path, cmd, len_src + len_cmd + 2);
+	return (path);
+}
+
+char	*find_path(char *cmd, char **envp)
 {
 	char	**dir;
-	char	*path;
+	char	*path_all;
+	char	*path_cmd;
 	int		i;
 
 	// loop through envp to find PATH
@@ -26,44 +60,50 @@ char	**find_path(char **envp)
 			break;
 		i++;
 	}
-	path = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+	// trim "PATH"
+	path_all = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+	
 	// split into directories using : separator
-	dir = ft_split(path, ':');
-	free(path);
-	return (dir);
+	dir = ft_split(path_all, ':');
+	free(path_all);
+
 	// check if file exists and is executable
+	i = 0;
+	while (dir[i])
+	{
+		path_cmd = build_path(dir[i], cmd);
+		if (access(path_cmd, X_OK) == 0 || !path_cmd)
+			return (free_tab(dir), path_cmd);
+		free(path_cmd);
+		i++;
+	}
+	return (free_tab(dir), NULL);
 }
+
 int main(int argc, char **argv, char **envp)
 {
-	// char	*cmd;
-    // char 	*file;
-    // char 	**args;
-	int	i;
-	char	**dir;
+	char	*path;
+    char 	*file;
+    char 	**args;
 	
 	if (argc < 3) {
         return 1;
     }
 
-	// cmd = find_path(argv, envp);
-	// file = argv[2];
-	// args[0] = cmd;
-	// args[1] = file;  // e.g., "infile"
+	path = find_path(argv[1], envp);
+	file = argv[2];
+	args = malloc(argc * sizeof(char *));
+	args[0] = path;
+	args[1] = file;  // e.g., "infile"
+	args[2] = NULL;
 
-    // // Try to execute
-    // execve(cmd, args, envp);
+    // Try to execute
+    execve(path, args, envp);
 
-    // // If execve returns, there was an error
-    // perror("execve");
-	ft_printf("%s\n", argv[1]);
-
-	dir = find_path(envp);
-	i = 0;
-	while (dir[i])
-	{
-		ft_printf("%s\n", dir[i]);
-		i++;
-	}
+    // If execve returns, there was an error
+    perror("execve");
+	free_tab(args);
+	free (path); // wait 
     return 1;
 }
 
