6673f297a7c5a6e52549a5d3cfe7d3cf79f943c7
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2025-09-09 17:47:03 +0200

    v0.6 infile cmd1 outfile

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cecompte <cecompte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 11:13:28 by cecompte          #+#    #+#             */
-/*   Updated: 2025/09/09 17:08:15 by cecompte         ###   ########.fr       */
+/*   Updated: 2025/09/09 17:44:48 by cecompte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,33 +43,45 @@ int main(int argc, char **argv, char **envp)
 {
 	int		status;
 	int		fd1;
+	int		fd2;
 	char	*path;
-    char 	**args;
-	pid_t	child1;
+    char 	**cmd1_args;
+	pid_t	parent;
 	
 	if (argc < 1) {
         return 1;
     } //change that part
-	child1 = fork();
-	if (child1 < 0)
+	
+	//
+	fd1 = open(argv[1], O_RDONLY); // + open error
+	fd2 = open(argv[3], O_CREAT | O_RDWR | O_TRUNC, 0644); // + open error
+	
+	// make a pipe + fds go in pipefd[0] and pipefd[1]
+
+	parent = fork();
+	
+	if (parent < 0)
 		return (write(2, strerror(errno), ft_strlen(strerror(errno))), 1);
-	args = ft_split(argv[2], ' '); // + malloc error
-	path = find_path(args[0], envp); // + malloc error
-	fd1 = open(argv[1], O_RDONLY);
-	if (!child1)
+
+	cmd1_args = ft_split(argv[2], ' '); // + malloc error
+	path = find_path(cmd1_args[0], envp); // + malloc error
+	if (!parent)
 	{
-		dup2(fd1, 0);
+		dup2(fd1, 0); // + protect if dup2 < 0
+		dup2(fd2, 1); // protect
 		close(fd1);
-		execve(path, args, envp);
+		close(fd2);
+		execve(path, cmd1_args, envp);
 		perror("execve");
 	}
 	else
 	{
 		close(fd1);
-		waitpid(child1, &status, 0);
+		close(fd2);
+		waitpid(parent, &status, 0);
 	}
 	free(path);
-	free_tab(args);
+	free_tab(cmd1_args);
     return 1;
 }
 
