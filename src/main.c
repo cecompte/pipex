dfd6306b55e14b59e864c4df03dd68079bab11a9
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2025-09-09 16:55:01 +0200

    v0.4.1 no leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: cecompte <cecompte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 11:13:28 by cecompte          #+#    #+#             */
-/*   Updated: 2025/09/09 16:50:03 by cecompte         ###   ########.fr       */
+/*   Updated: 2025/09/09 16:54:31 by cecompte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,10 +52,8 @@ int main(int argc, char **argv, char **envp)
 	child1 = fork();
 	if (child1 < 0)
 		return (write(2, strerror(errno), ft_strlen(strerror(errno))), 1);
-	args = ft_split(argv[1], ' ');
-	path = find_path(args[0], envp);
-	if (!path)
-			return (!ft_printf("Command not found"));
+	args = ft_split(argv[1], ' '); // + malloc error
+	path = find_path(args[0], envp); // + malloc error
 	if (!child1)
 	{
 		execve(path, args, envp);
@@ -66,7 +64,7 @@ int main(int argc, char **argv, char **envp)
 		waitpid(child1, &status, 0);
 	}
 	free(path);
-	free(args);
+	free_tab(args);
     return 1;
 }
 
